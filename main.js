94c16d55398fbc91bf8990f64d8382deb5f7be74
Ingridmasba2007 <127319046+Ingridmasba2007@users.noreply.github.com>
2024-07-24 09:28:28 -0300

    .

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto')
 const botoes= document.querySelectorAll ('.parametro-senha__botao')
-connsole.log(botoes)
+console.log(botoes)
 
 
 
