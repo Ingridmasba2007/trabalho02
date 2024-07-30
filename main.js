ae32ff23cf97ecb30a88c66d83f04630eed3b0e2
Ingridmasba2007 <127319046+Ingridmasba2007@users.noreply.github.com>
2024-07-30 07:46:02 -0300

    ty

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto')
 const botoes= document.querySelectorAll ('.parametro-senha__botao')
 botoes[0].onclick = diminuirTamanho
-let tamanhoSenha = 5 
+let tamanhoSenha = 5
+numeroSenha.textContent = tamanhoSenha
 
 function diminuirTamanho(){
    tamanhoSenha = tamanhoSenha - 1
