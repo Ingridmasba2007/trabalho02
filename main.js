85a51be178c8bb1893b2d219508529aea55c9a2b
Ingridmasba2007 <127319046+Ingridmasba2007@users.noreply.github.com>
2024-07-30 07:39:19 -0300

    uk

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto')
 const botoes= document.querySelectorAll ('.parametro-senha__botao')
 botoes[0].onclick = diminuirTamanho
-function  diminuirTamanho () {
+function diminuirTamanho(){
     console.log('teste')
 }
 
