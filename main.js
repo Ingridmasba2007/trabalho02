36f25ab50c1c6b8ce7f359f63103d8917ee3f331
Ingridmasba2007 <127319046+Ingridmasba2007@users.noreply.github.com>
2024-08-07 09:30:55 -0300

    ;

--- main.js
+++ main.js
@@ -34,6 +34,7 @@ function geraSenha(){
   let senha = " "
   for(let i =0;i < tamanhoSenha; i++) {
    let numeroAleatorio = Math.random()*maiuscula.length
+   numeroAleatorio = Math.floor(numeroAleatorio)
    console.log(numeroAleatorio) 
 }
 }
